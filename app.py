cb972bf3faff5ef62d9d6dcef14d51863ef236fc
Michael Geliebter <michaelgeliebter4180@gmail.com>
2025-04-08 19:48:26 -0700

    Restore full app with routes and templates

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Render!"
+    return render_template('home.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/pitch')
+def pitch():
+    return render_template('pitch.html')
+
+@app.route('/engagements')
+def engagements():
+    return render_template('engagements.html')
+
+@app.route('/reporting-examples')
+def examples():
+    return render_template('examples.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/test')
+def test():
+    return "It works!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
