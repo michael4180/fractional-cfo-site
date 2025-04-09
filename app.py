19156c265d48e6b6045ad6b5e5a478c021c2568c
Michael Geliebter <michaelgeliebter4180@gmail.com>
2025-04-08 18:56:39 -0700

    Test: minimal Flask route to debug Render

--- app.py
+++ app.py
@@ -1,34 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-@app.route('/services')
-def services():
-    return render_template('services.html')
-
-@app.route('/pitch')
-def pitch():
-    return render_template('pitch.html')
-
-@app.route('/engagements')
-def engagements():
-    return render_template('engagements.html')
-
-@app.route('/reporting-examples')
-def examples():
-    return render_template('examples.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/test')
-def test():
-    return "It works!"
+    return "Hello from Render!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
